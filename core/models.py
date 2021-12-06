fb52ed682320f86ff405101cddaa9eb5c5bb4bcc
Xramoi2197 <gitarist2197@gmail.com>
2021-12-06 21:32:04 +0300

    Models create

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
+from datetime import datetime
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Tag(models.Model):
+    tag_name = models.CharField(max_length=100, unique=True)
+
+    def __str__(self) -> str:
+        return self.tag_name
+
+
+class Article(models.Model):
+    title = models.CharField(max_length=200, null=False)
+    url = models.URLField(null=False, unique=True)
+    create_date = models.DateTimeField(default=datetime.now(), null=False)
+    is_read = models.BooleanField(default=False, null=False)
+    like = models.BooleanField(null=True)
+    last_show_date = models.DateTimeField(default=datetime.now(), null=False)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self) -> str:
+        return self.title
