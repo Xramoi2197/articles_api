0df64fba0e3dfb2329524cdf193c811fcc247b82
Xramoi2197 <gitarist2197@gmail.com>
2021-12-08 20:39:59 +0300

    Add Articles to admin

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
+from .models import Tag, Article
+
 # Register your models here.
+class TagsInline(admin.StackedInline):
+    model = Article.tags.through
+    extra = 1
+
+
+class ArticleAdmin(admin.ModelAdmin):
+    search_fields = ["title", "url"]
+    list_display = (
+        "id",
+        "title",
+        "url",
+        "user",
+        "create_date",
+        "last_show_date",
+        "is_read",
+        "like",
+    )
+    list_filter = ["tags", "create_date"]
+    inlines = [TagsInline]
+    exclude = ("tags",)
+
+
+admin.site.register(Article, ArticleAdmin)
