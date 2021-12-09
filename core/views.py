54b5f7879e40de527d4118ebfd6e178886b2474c
Xramoi2197 <gitarist2197@gmail.com>
2021-12-09 22:55:16 +0300

    Add pagination

--- core/views.py
+++ core/views.py
@@ -1,9 +1,18 @@
-from rest_framework import viewsets
+from rest_framework import viewsets, pagination
 
 from .serializers import ArticleSerializer
 from .models import Article
+
+
+class ArticleViewPagination(pagination.PageNumberPagination):
+    page_size = 10
+    page_size_query_param = "page_size"
+    ordering = "create_date"
+    max_page_size = 50
+
 
 # Create your views here.
 class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
+    pagination_class = ArticleViewPagination
