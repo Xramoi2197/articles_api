693fd40af9c4ead1799cae83fdc1416a7068e9e5
Xramoi2197 <gitarist2197@gmail.com>
2021-12-15 21:54:12 +0300

    Add articles by tag request

--- core/urls.py
+++ core/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import ArticleViewSet
+from .views import ArticleViewSet, ArticleByTagDetailView
 
 router = DefaultRouter()
 router.register('articles', ArticleViewSet, basename='articles')
 
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("tags/<slug:tag_name>/", ArticleByTagDetailView.as_view()),
 ]
--- core/views.py
+++ core/views.py
@@ -1,18 +1,28 @@
-from rest_framework import viewsets, pagination
+from rest_framework import generics, viewsets, pagination
 
 from .serializers import ArticleSerializer
-from .models import Article
+from .models import Article, Tag
 
 
-class ArticleViewPagination(pagination.PageNumberPagination):
+class ArticlePagination(pagination.PageNumberPagination):
     page_size = 10
     page_size_query_param = "page_size"
     ordering = "create_date"
     max_page_size = 50
 
 
-# Create your views here.
 class ArticleViewSet(viewsets.ModelViewSet):
     serializer_class = ArticleSerializer
     queryset = Article.objects.all()
-    pagination_class = ArticleViewPagination
+    pagination_class = ArticlePagination
+
+
+class ArticleByTagDetailView(generics.ListAPIView):
+    serializer_class = ArticleSerializer
+    pagination_class = ArticlePagination
+    slug_field = 'tag_name'
+
+    def get_queryset(self):
+        tag_name = self.kwargs['tag_name'].lower()
+        tag = Tag.objects.get(tag_name=tag_name)
+        return Article.objects.filter(tags=tag)
